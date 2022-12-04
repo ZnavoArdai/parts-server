e835db275d09993035e1050caaf73bc296871b16
ZnavoArdai <102303011+ZnavoArdai@users.noreply.github.com>
2022-12-04 17:06:30 +0200

    d

--- server/server.js
+++ server/server.js
@@ -33,9 +33,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {});
 console.log(`listening to port ${port}`);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../server/node server.js')));
-  app.get('*', (req, res)=>{
-      res.sendFile(path.join(__dirname, '../server/npm install', 'server.js'));
-  });
-}
